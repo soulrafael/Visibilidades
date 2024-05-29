27a1caf2139a53f857250e27625d04f9122903ac
soulrafael <rafaelbsoul@gmail.com>
2024-05-28 23:57:07 -0300

    first commit

---
+++ src/Atendente/Atendente.java
@@ -0,0 +1,19 @@
+package Atendente;
+
+
+public class Atendente {
+    public String nome;
+    String numero;
+
+  
+     private static int beberAgua(){
+        int soma = 1;
+        System.out.println(soma);
+        return soma;
+    }
+    public static void atender(){
+        int testador= beberAgua();
+        System.out.println("Bom dia, meu nome é  "+testador);
+    }
+    
+}
---
+++ src/Estabelecimento.java
@@ -0,0 +1,13 @@
+import Atendente.Atendente;
+
+public class Estabelecimento {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Estabelecimento INVISIVEL");
+        Atendente atendente1 = new Atendente();
+        atendente1.nome="Guilberte";
+        atendente1.atender();
+        
+    
+    
+    }
+}
