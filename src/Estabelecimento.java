52f35efbbc648f83c183cab264eec8387c02a0e8
soulrafael <rafaelbsoul@gmail.com>
2024-05-28 23:58:53 -0300

    Observações

--- src/Estabelecimento.java
+++ src/Estabelecimento.java
@@ -5,7 +5,10 @@ public class Estabelecimento {
         System.out.println("Estabelecimento INVISIVEL");
         Atendente atendente1 = new Atendente();
         atendente1.nome="Guilberte";
-        atendente1.atender();
+        atendente1.atender(); 
+        // A visibilidade private dentro da mesma classe 
+        // default dentro do mesmo pacote 
+        // private pode ser usado em operaçoes que não é necessario ser vista na classe principal
         
     
     
